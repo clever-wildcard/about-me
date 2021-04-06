7528f60fdb26782a74756cfbd648cf81e27e6347
gandalf <gandalf@gandalfs-MacBook-Pro.local>
2021-04-05 21:30:43 -0500

    Adding spring boot files

---
+++ src/test/java/com/cleverwildcard/aboutme/AboutMeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cleverwildcard.aboutme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AboutMeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
