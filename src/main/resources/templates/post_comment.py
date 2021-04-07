85168a75ff2da4add14a7a6bd412c52b0b563b3f
gandalf <gandalf@gandalfs-MacBook-Pro.local>
2021-04-06 21:57:20 -0500

    AWS would go next

--- src/main/resources/templates/post_comment.py
+++
@@ -1,4 +0,0 @@
-import requests
-import sys
-
-response = requests.post('http://locahost:8080/comments', json={'author': sys.argv[0], 'comment': sys.argv[1]})
--- test_rest.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-response = requests.post("http://localhost:8080/comments", json={"author": "me", "content": "this is a comment"})
-print(response.status_code)
-response = requests.post("http://localhost:8080/comments", json={"author": "", "content": "this is a comment"})
-print(response.status_code)
-print(requests.get("http://localhost:8080/comments").json())
